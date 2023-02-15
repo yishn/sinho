cb70f38a2fe6a83a1c33d98dc81c38453558d7e0
Yichuan Shen <shen.yichuan@gmail.com>
2023-02-15 20:48:16 +0100

    Some optimizations

--- src/renderer/for.ts
+++ src/renderer/for.ts
@@ -24,7 +24,7 @@ function diff<K>(
   to: K[]
 ): [ops: ArrayOp[], valueOps: Map<K, ArrayOp>] {
   const valueOps = new Map<K, ArrayOp>();
-  const temp = [...from];
+  let needMove = false;
 
   for (let i = 0; i < from.length; i++) {
     valueOps.set(from[i], [ArrayOpType.Removed, i]);
@@ -34,19 +34,21 @@ function diff<K>(
     const oldEntry = valueOps.get(to[j]);
 
     if (oldEntry != null) {
+      needMove = true;
       valueOps.set(to[j], [ArrayOpType.Move, oldEntry[1], j]);
     } else {
       valueOps.set(to[j], [ArrayOpType.Added, j]);
     }
   }
 
+  const temp = needMove ? [...from] : null;
   const ops: ArrayOp[] = [];
 
   for (let i = from.length - 1; i >= 0; i--) {
     const entry = valueOps.get(from[i]);
 
     if (entry?.[0] === ArrayOpType.Removed) {
-      temp.splice(i, 1);
+      temp?.splice(i, 1);
       ops.push(entry);
     }
   }
@@ -55,17 +57,17 @@ function diff<K>(
     const entry = valueOps.get(to[j]);
 
     if (entry?.[0] === ArrayOpType.Added) {
-      temp.splice(j, 0, to[j]);
+      temp?.splice(j, 0, to[j]);
       ops.push(entry);
     } else if (entry?.[0] === ArrayOpType.Move) {
-      const [, i, j] = entry;
-      const tempIndex = temp.indexOf(to[j]);
+      const [, , j] = entry;
+      const tempIndex = temp?.indexOf(to[j], j) ?? -1;
 
       if (tempIndex !== j) {
         // Move needed
 
-        temp.splice(tempIndex, 1);
-        temp.splice(j, 0, to[j]);
+        temp?.splice(tempIndex, 1);
+        temp?.splice(j, 0, to[j]);
       }
 
       ops.push([ArrayOpType.Move, tempIndex, j]);
@@ -85,27 +87,27 @@ interface StateEntry<R extends Renderer, T> {
 
 interface ForProps<T, K> {
   source: SignalLike<T[]>;
-  keyFn: (value: T, index: number) => K;
-  eachFn: (value: Signal<T>, index: Signal<number>) => Component;
+  key: (value: T, index: number) => K;
+  each: (value: Signal<T>, index: Signal<number>) => Component;
 }
 
 export class ForComponent<T, K> extends Component<ForProps<T, K>> {
   constructor(source: SignalLike<T[]>) {
     super({
       source,
-      keyFn: (_, i) => i as K,
-      eachFn: (_, __) => new Fragment({}),
+      key: (_, i) => i as K,
+      each: (_, __) => new Fragment({}),
     });
   }
 
   key<K>(keyFn: (value: T, index: number) => K): ForComponent<T, K> {
     const self = this as unknown as ForComponent<T, K>;
-    self.props.keyFn = keyFn;
+    self.props.key = keyFn;
     return self;
   }
 
   each(eachFn: (value: Signal<T>, index: Signal<number>) => Component): this {
-    this.props.eachFn = eachFn;
+    this.props.each = eachFn;
     return this;
   }
 
@@ -113,10 +115,7 @@ export class ForComponent<T, K> extends Component<ForProps<T, K>> {
     let firstTime = true;
 
     const endMarker: RendererNode<R> = s.renderer.createMarkerNode();
-    const rendering: [
-      list: [marker: RendererNode<R>, rendering: Rendering<R>][],
-      marker: RendererNode<R>
-    ] = [[], endMarker];
+    const rendering: [marker: RendererNode<R>, rendering: Rendering<R>][] = [];
 
     const state = new Map<K, StateEntry<R, T>>();
     let keys: K[] = [];
@@ -124,7 +123,7 @@ export class ForComponent<T, K> extends Component<ForProps<T, K>> {
     s.effect(() => {
       const newKeys = this.props
         .source()
-        .map((value, i) => this.props.keyFn(value, i));
+        .map((value, i) => this.props.key(value, i));
       const [ops] = diff(keys, newKeys);
 
       for (const op of ops) {
@@ -148,7 +147,7 @@ export class ForComponent<T, K> extends Component<ForProps<T, K>> {
               ] = [
                 marker,
                 this.props
-                  .eachFn(value, index)
+                  .each(value, index)
                   .createRenderingWithDestructor(s)[0],
               ];
 
@@ -164,11 +163,11 @@ export class ForComponent<T, K> extends Component<ForProps<T, K>> {
               if (!firstTime) {
                 // Insert rendering
 
-                const beforeMarker = rendering[0][j]?.[0] ?? endMarker;
+                const beforeMarker = rendering[j]?.[0] ?? endMarker;
                 s.renderer.insertRendering(eachRendering, beforeMarker);
               }
 
-              rendering[0].splice(j, 0, eachRendering);
+              rendering.splice(j, 0, eachRendering);
             },
             { leaked: true }
           );
@@ -179,7 +178,7 @@ export class ForComponent<T, K> extends Component<ForProps<T, K>> {
           const key = keys[i];
           const entry = state.get(key)!;
 
-          rendering[0].splice(i, 1);
+          rendering.splice(i, 1);
           entry.destructor();
           state.delete(key);
         } else if (op[0] === ArrayOpType.Move) {
@@ -188,11 +187,11 @@ export class ForComponent<T, K> extends Component<ForProps<T, K>> {
           const entry = state.get(key)!;
 
           if (tempIndex !== j) {
-            const beforeMarker = rendering[0][j]?.[0] ?? endMarker;
-            const [eachRendering] = rendering[0].splice(tempIndex, 1);
+            const beforeMarker = rendering[j]?.[0] ?? endMarker;
+            const [eachRendering] = rendering.splice(tempIndex, 1);
 
             s.renderer.insertRendering(eachRendering, beforeMarker);
-            rendering[0].splice(j, 0, eachRendering);
+            rendering.splice(j, 0, eachRendering);
           }
 
           entry.setIndex(j);
@@ -209,7 +208,7 @@ export class ForComponent<T, K> extends Component<ForProps<T, K>> {
       }
     });
 
-    return rendering;
+    return [rendering, endMarker];
   }
 }
 
