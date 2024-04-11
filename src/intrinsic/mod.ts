f92e9b80fd584c7f07ede3ef9cc7a6da70cadf81
Yichuan Shen <shen.yichuan@gmail.com>
2024-04-11 19:10:45 +0200

    Remove ErrorBoundary

--- src/intrinsic/mod.ts
+++ src/intrinsic/mod.ts
@@ -1,5 +1,4 @@
 export { Dynamic } from "./Dynamic.js";
-export { ErrorBoundary, useErrorBoundary } from "./ErrorBoundary.js";
 export { For } from "./For.js";
 export { type Children, Fragment } from "./Fragment.js";
 export { If } from "./If.js";
