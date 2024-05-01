f1aa9fcdd983c904b3601411181775315c1db88d
Yichuan Shen <shen.yichuan@gmail.com>
2024-05-01 18:24:12 +0200

    Fix

--- web/src/components/monacoEditor.tsx
+++ web/src/components/monacoEditor.tsx
@@ -1,6 +1,6 @@
 import { useColorMode } from "@docusaurus/theme-common";
 import useBaseUrl from "@docusaurus/useBaseUrl";
-import * as monaco from "monaco-editor";
+import type * as monaco from "monaco-editor";
 import { CSSProperties, FC, useEffect, useRef } from "react";
 
 export const MonacoEditor: FC<{
@@ -14,56 +14,61 @@ export const MonacoEditor: FC<{
   const { colorMode } = useColorMode();
 
   useEffect(() => {
-    editorRef.current = monaco.editor.create(divRef.current!, {
-      model: monaco.editor.createModel(
-        props.text!,
-        "typescript",
-        monaco.Uri.file("/main.tsx"),
-      ),
-      language: "typescript",
-      minimap: {
-        enabled: false,
-      },
-      smoothScrolling: true,
-      automaticLayout: true,
-    });
+    (async () => {
+      const monaco = await import("monaco-editor");
 
-    editorRef.current.onDidChangeModelContent(() => {
-      props.onChange?.(editorRef.current!.getValue());
-    });
-
-    monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
-      strict: true,
-      jsx: monaco.languages.typescript.JsxEmit.ReactJSX,
-      jsxImportSource: "shingo",
-    });
-
-    fetch(typesPath)
-      .then((res) => res.text())
-      .then((content) => {
-        monaco.languages.typescript.typescriptDefaults.addExtraLib(
-          `declare module "shingo" {\n${content}\n}`,
-          "ts:shingo.d.ts",
-        );
-        monaco.languages.typescript.typescriptDefaults.addExtraLib(
-          `declare module "shingo/jsx-runtime" {\n${content}\n}`,
-          "ts:shingo-jsx-runtime.d.ts",
-        );
+      editorRef.current = monaco.editor.create(divRef.current!, {
+        model: monaco.editor.createModel(
+          props.text!,
+          "typescript",
+          monaco.Uri.file("/main.tsx"),
+        ),
+        language: "typescript",
+        minimap: {
+          enabled: false,
+        },
+        smoothScrolling: true,
+        automaticLayout: true,
       });
 
+      editorRef.current.onDidChangeModelContent(() => {
+        props.onChange?.(editorRef.current!.getValue());
+      });
+
+      monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
+        strict: true,
+        jsx: monaco.languages.typescript.JsxEmit.ReactJSX,
+        jsxImportSource: "shingo",
+      });
+
+      const types = await fetch(typesPath).then((res) => res.text());
+
+      monaco.languages.typescript.typescriptDefaults.addExtraLib(
+        `declare module "shingo" {\n${types}\n}`,
+        "ts:shingo.d.ts",
+      );
+
+      monaco.languages.typescript.typescriptDefaults.addExtraLib(
+        `declare module "shingo/jsx-runtime" {\n${types}\n}`,
+        "ts:shingo-jsx-runtime.d.ts",
+      );
+    })();
+
     return () => {
-      editorRef.current!.getModel()!.dispose();
-      editorRef.current!.dispose();
+      editorRef.current?.getModel()!.dispose();
+      editorRef.current?.dispose();
     };
   }, []);
 
   useEffect(() => {
-    monaco.editor.setTheme(colorMode == "light" ? "vs" : "vs-dark");
+    import("monaco-editor").then((monaco) =>
+      monaco.editor.setTheme(colorMode == "light" ? "vs" : "vs-dark"),
+    );
   }, [colorMode]);
 
   useEffect(() => {
-    if (editorRef.current!.getValue() != props.text) {
-      editorRef.current!.setValue(props.text!);
+    if (editorRef.current?.getValue() != props.text) {
+      editorRef.current?.setValue(props.text!);
     }
   }, [props.text]);
 
